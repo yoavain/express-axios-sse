d70462e8678de71c902e266c6abf493b648729e7
Yoav Vainrich <yoavain@gmail.com>
2023-05-27 11:30:28 +0300

    Replace GET with POST

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const { loremIpsum } = require("lorem-ipsum");
 
 async function run() {
@@ -8,37 +8,36 @@ async function run() {
 
     app.use(bodyParser.json());
 
-    app.post('/events', async function(req, res) {
+    app.post("/events", async function(req, res) {
         const body = req.body;
-        console.log('Got /events', 'with id', body);
+        console.log("Got /events", "with id", body.id);
         res.set({
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'text/event-stream',
-            'Connection': 'keep-alive'
+            "Cache-Control": "no-cache",
+            "Content-Type": "text/event-stream",
+            "Connection": "keep-alive"
         });
         res.flushHeaders();
 
         // Tell the client to retry every 10 seconds if connectivity is lost
-        res.write('retry: 10000\n\n');
+        res.write("retry: 10000\n\n");
         let count = 0;
 
         while (true) {
             await new Promise(resolve => setTimeout(resolve, 1000));
 
-            // Emit an SSE that contains the current 'count' as a string
             const data = loremIpsum({
                 count: 20,
                 units: "sentences",
                 format: "plain",
                 suffix: "\n"
             });
-            console.log('Emit #', ++count, 'Sent: ', data.length, 'bytes');
+            console.log("Emit #", ++count, "Sent: ", data.length, "bytes");
             res.write(`data: ${data}\n\n`);
         }
     });
 
     await app.listen(3000);
-    console.log('Listening on port 3000');
+    console.log("Listening on port 3000");
 }
 
 
